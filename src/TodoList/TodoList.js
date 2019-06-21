9077b365dd6cfd1bf30e6d22a2ae67158d41b099
W <huiru.wang02>
2019-06-21 16:59:47 +0800

    antd-todoList

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,14 +1,12 @@
 import React, { Component,Fragment} from 'react';
               // 解构   
-import {Form, Input, Button, Card} from 'antd' 
-import ToggleUser from "../User/ToggleUser";
-import TodoItem from "./TodoItem";
-// import axios from 'axios';
-import './todolist.css';   // 引入CSS文件
+import {Form, Input, Button, Card, List, Typography} from 'antd' 
+import { CSSTransition } from 'react-transition-group'
+import './todolist.less';   // 引入CSS文件
 
 class TodoList extends React.Component{
     state={
-        item:['自由']
+        item:['自由','自律']
 
     }
     renderForm=()=>{
@@ -37,14 +35,70 @@ class TodoList extends React.Component{
 
         )
     }
+    renderTodoItem=()=>{
+        const { item }=this.state
+        const r = Math.round(Math.random()*255);
+ 
+        // const g = Math.round(Math.random()*255);
+        // const b = Math.round(Math.random()*255);
+        // const a = ( (Math.random()*5 + 5) / 10 ).toFixed(2)
+        // const color = `rgba(${r},${g},${b},${a})`
+        
+        return(
+            
+                <List
+                    bordered
+                    dataSource={item}
+                    renderItem={(item, index) => (
+                        <CSSTransition
+                            in={true}
+                            // 第一次展示时也有动画效果
+                            appear={true}
+                            // 动画要执行多久
+                            timeout={300}
+                            classNames='fade'
+                            //in=false时  该DOM节点被移除
+                            // unmountOnExit
+                            // 钩子函数
+                            onEntered={(e)=>{
+                                switch(index % 3){
+                                    case 0:
+                                        e.style.color='#1DA57A'
+                                        break;
+                                    case 1:
+                                        e.style.color='rgb(230, 139, 22)'
+                                        break;
+                                    case 2:
+                                        e.style.color='#d80919'
+                                        break;
+
+                                }
+                            }}
+                        >
+                            <List.Item>
+                                <Typography.Text mark>[今日待办]</Typography.Text> {item}
+                            </List.Item>
+                         </CSSTransition>
+                    )}
+                />
+           
+        )
+    }
 
     render(){
-        const { item }=this.state
+        
+
+        
         return(
-            <Card>
-                {this.renderForm()}
-                <TodoItem item={item}/>
+            <Card
+                title={this.renderForm()}
+            >
+               
+                {this.renderTodoItem()}
+               
+                
             </Card>
+            
             
 
         )
