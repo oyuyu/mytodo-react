1750b00cd6efd7147a09c9207c89dedf30643590
W <huiru.wang02>
2019-07-18 11:15:17 +0800

    W

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // 加载TodoList文件
 // import TodoList from './TodoList/TodoList';
-import { AutoGetStaff } from '../TodoList/index';
+import  { AutoGetStaff} from '../TodoList/TodoList';
 
 export default class APP extends React.Component {
     render(){
