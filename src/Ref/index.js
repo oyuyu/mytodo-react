cddd901e5635201d4ee9647e36671b64d40cc302
oyuyu <370747113@qq.com>
2019-11-06 15:58:30 +0800

    Table组件

--- src/Ref/index.js
+++ src/Ref/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// React.createRef
 class TextInput extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -17,22 +18,6 @@ class TextInput extends React.PureComponent {
         <input type="button" value="点击聚焦" onClick={this.focusTextInput} />
       </div>
     );
-  }
-}
-
-// 为组件添加Ref
-class AutoFocus extends React.Component {
-  constructor(props) {
-    super(props);
-    // 创建一个ref存储textInput的DOM元素
-    this.textInput = React.createRef();
-  }
-  componentDidMount() {
-    this.textInput.current.focusTextInput();
-  }
-
-  render() {
-    return <TextInput ref={this.textInput} />;
   }
 }
 
@@ -62,7 +47,7 @@ class CustomText extends React.Component {
   }
 }
 
-export default class InputRefs extends React.Component {
+export class InputRefs extends React.Component {
   constructor(props) {
     super(props);
     this.textInput = null;
@@ -77,8 +62,6 @@ export default class InputRefs extends React.Component {
     if (this.textInput) this.textInput.focus();
     // 组件间传值 方式2
     console.log(this.refs.textInput2.value);
-    
-
   };
   render() {
     return (
@@ -94,7 +77,44 @@ export default class InputRefs extends React.Component {
         />
         {/* 组件间传值   方式2 */}
         <input type="text"  ref='textInput2' />
+        <input type="button" value="聚焦" onClick={this.focus} />
+      </div>
+    );
+  }
+}
 
+
+// React.forwordRefs()
+export default class ForwordRefs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = null;
+  }
+  componentDidMount() {
+    // 组件挂载后 文本框自动获取焦点
+    this.focus();
+  }
+  focus = () => {
+    console.log(this.textInput.value, this.p.innerText);
+    // 组件间传值 方式1
+    if (this.textInput) this.textInput.focus();
+    // 组件间传值 方式2
+    console.log(this.refs.textInput2.value);
+  };
+  render() {
+    return (
+      <div>
+        {/* 组件间传值方式1 */}
+        <p ref={p => { this.p = p}}> refs组件间传递 </p>
+        <input
+          type="text"
+          // 相当于将其本身通过形参传给 组件的setInputRef
+          ref={ipt => {
+            this.textInput = ipt;
+          }}
+        />
+        {/* 组件间传值   方式2 */}
+        <input type="text"  ref='textInput2' />
         <input type="button" value="聚焦" onClick={this.focus} />
       </div>
     );
