584c7c766b5e8fba1d2fc12003403431f75f35ec
oyuyu <370747113@qq.com>
2020-07-12 16:29:56 +0800

    init 算法相关

---
+++ src/App/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom"; //项目运行的核心
+
+const App = () => {
+	return <div>算法大全</div>;
+};
+export default App;
--- src/HOC/README.md
+++
@@ -1,3 +0,0 @@
-高阶组件是参数为组件，返回值为新组件的函数
-
-在 React 的第三方库中很常见，例如 Redux 的 connect 和 Relay 的 createFragmentContainer
